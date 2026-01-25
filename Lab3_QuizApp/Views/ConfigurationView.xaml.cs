6bbd218118827a37f3226b26a710d06969a51b8c
Whezzan <d.skonneberg@gmail.com>
2026-01-25 15:37:25 +0100

    Add Play/Edit buttons to ResultView. Add BIG PLAY BUTTON to ConfigurationView.

--- Lab3_QuizApp/Views/ConfigurationView.xaml.cs
+++ Lab3_QuizApp/Views/ConfigurationView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using QuizAppExtended.ViewModels;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 
 namespace QuizAppExtended.Views
@@ -16,6 +18,47 @@ namespace QuizAppExtended.Views
             if (DataContext is ConfigurationViewModel vm)
             {
                 vm.ScheduleAutoSave();
+            }
+        }
+
+        private async void OnPlayClick(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow?.DataContext is not MainWindowViewModel mainVm)
+            {
+                return;
+            }
+
+            CommitAllTextBoxes(Application.Current.MainWindow);
+
+            await mainVm.ConfigurationViewModel.FlushAutoSaveAsync();
+
+            if (mainVm.PlayerViewModel.SwitchToPlayModeCommand.CanExecute(null))
+            {
+                mainVm.PlayerViewModel.SwitchToPlayModeCommand.Execute(null);
+            }
+
+            e.Handled = true;
+        }
+
+        private static void CommitAllTextBoxes(DependencyObject root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            var count = VisualTreeHelper.GetChildrenCount(root);
+            for (int i = 0; i < count; i++)
+            {
+                var child = VisualTreeHelper.GetChild(root, i);
+
+                if (child is TextBox tb)
+                {
+                    var expr = tb.GetBindingExpression(TextBox.TextProperty);
+                    expr?.UpdateSource();
+                }
+
+                CommitAllTextBoxes(child);
             }
         }
     }
--- Lab3_QuizApp/Views/ResultView.xaml.cs
+++ Lab3_QuizApp/Views/ResultView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using QuizAppExtended.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 
 namespace QuizAppExtended.Views
@@ -11,6 +14,47 @@ namespace QuizAppExtended.Views
         public ResultView()
         {
             InitializeComponent();
+        }
+
+        private async void OnPlayClick(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow?.DataContext is not MainWindowViewModel mainVm)
+            {
+                return;
+            }
+
+            CommitAllTextBoxes(Application.Current.MainWindow);
+
+            await mainVm.ConfigurationViewModel.FlushAutoSaveAsync();
+
+            if (mainVm.PlayerViewModel.SwitchToPlayModeCommand.CanExecute(null))
+            {
+                mainVm.PlayerViewModel.SwitchToPlayModeCommand.Execute(null);
+            }
+
+            e.Handled = true;
+        }
+
+        private static void CommitAllTextBoxes(DependencyObject root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            var count = VisualTreeHelper.GetChildrenCount(root);
+            for (int i = 0; i < count; i++)
+            {
+                var child = VisualTreeHelper.GetChild(root, i);
+
+                if (child is TextBox tb)
+                {
+                    var expr = tb.GetBindingExpression(TextBox.TextProperty);
+                    expr?.UpdateSource();
+                }
+
+                CommitAllTextBoxes(child);
+            }
         }
     }
 }
