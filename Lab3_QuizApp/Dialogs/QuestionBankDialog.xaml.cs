52ed5d9ff0f5077f6d1d05d7e5f3e4db734dc3a6
Erik Holgersson <erik.holgersson95@gmail.com>
2026-01-26 10:05:13 +0100

    Fix bug where you could only add 1 question to a pack in QuestionBankDialog

--- Lab3_QuizApp/Dialogs/QuestionBankDialog.xaml.cs
+++ Lab3_QuizApp/Dialogs/QuestionBankDialog.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using QuizAppExtended.Models;
+using QuizAppExtended.ViewModels;
 
 namespace QuizAppExtended.Dialogs
 {
@@ -7,6 +10,25 @@ namespace QuizAppExtended.Dialogs
         public QuestionBankDialog()
         {
             InitializeComponent();
+        }
+
+        private void OnSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (DataContext is not MainWindowViewModel vm)
+            {
+                return;
+            }
+
+            if (e.NewValue is Question q)
+            {
+                vm.SelectedBankQuestion = q;
+            }
+            else
+            {
+                vm.SelectedBankQuestion = null;
+            }
+
+            vm.AddQuestionFromBankCommand.RaiseCanExecuteChanged();
         }
     }
 }
